858f72eb9e1a95a45421ea311b1b67e03a5b877d
Lyrakun <273812235+Lyrakunai@users.noreply.github.com>
2026-05-04 12:12:22 +0530

    Reformat reverse_guess_game.py for consistency

--- reverse_guess_game.py
+++ reverse_guess_game.py
@@ -1,12 +1,10 @@
 low = 1
 high = 50
-
 print("Think of a number between 1 and 50")
 while True:
     guess = (low + high) // 2
     print("Is your number", guess, "?")
     feedback = input("Enter high / low / correct: ")
-    
     if feedback == "high":
         high = guess - 1
     elif feedback == "low":
